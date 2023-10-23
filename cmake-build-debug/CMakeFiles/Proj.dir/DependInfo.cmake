bc3d48eeb309afc7676e0cdf9db3bbf97ae966f7
Tristan Potgieter <tristanpotgieter2001@gmail.com>
2023-10-23 14:02:12 +0200

    Switched Branch and started StockAndFinance module

---
+++ cmake-build-debug/CMakeFiles/Proj.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Tristan/Documents/UP/Semseter 2 2023/COS214/Proj/root/StockAndFinance/Finance.cpp" "/mnt/c/Users/Tristan/Documents/UP/Semseter 2 2023/COS214/Proj/cmake-build-debug/CMakeFiles/Proj.dir/root/StockAndFinance/Finance.cpp.o"
+  "/mnt/c/Users/Tristan/Documents/UP/Semseter 2 2023/COS214/Proj/root/StockAndFinance/Shelf.cpp" "/mnt/c/Users/Tristan/Documents/UP/Semseter 2 2023/COS214/Proj/cmake-build-debug/CMakeFiles/Proj.dir/root/StockAndFinance/Shelf.cpp.o"
+  "/mnt/c/Users/Tristan/Documents/UP/Semseter 2 2023/COS214/Proj/root/StockAndFinance/Stock.cpp" "/mnt/c/Users/Tristan/Documents/UP/Semseter 2 2023/COS214/Proj/cmake-build-debug/CMakeFiles/Proj.dir/root/StockAndFinance/Stock.cpp.o"
+  "/mnt/c/Users/Tristan/Documents/UP/Semseter 2 2023/COS214/Proj/root/StockAndFinance/StockAndFinanceFacade.cpp" "/mnt/c/Users/Tristan/Documents/UP/Semseter 2 2023/COS214/Proj/cmake-build-debug/CMakeFiles/Proj.dir/root/StockAndFinance/StockAndFinanceFacade.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../root/StockAndFinance"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ root/StockAndFinance/Shelf.h
@@ -0,0 +1,20 @@
+//
+// Created by Tristan on 2023/10/23.
+//
+
+#ifndef PROJ_SHELF_H
+#define PROJ_SHELF_H
+
+
+class Shelf
+{
+public:
+
+private:
+    List<Stock> stockList;
+    int currentCapacity;
+    int maxCapacity;
+};
+
+
+#endif //PROJ_SHELF_H
