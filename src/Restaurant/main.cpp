015858f9bc2eee58a3f0d227c78f585a7c5c3534
Monika Theiss <131775579+BubbleQueenzy@users.noreply.github.com>
2023-10-25 11:52:29 +0200

    add(main)

---
+++ src/Restaurant/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../kitchen/Kitchen.h"
+#include "../customer/Person.h"
+using namespace std;
+int main(){
+    Person p;
+    Kitchen k;
+
+    p.sayHello();
+    k.sayHello();
+}
