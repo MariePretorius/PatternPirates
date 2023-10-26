ff4614a945f781c0d33a29365e0a1f9b354d1041
Monika Theiss <131775579+BubbleQueenzy@users.noreply.github.com>
2023-10-26 11:41:48 +0200

    Update main.cpp(removed_Person)

--- src/Restaurant/main.cpp
+++ src/Restaurant/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include "../Kitchen/Kitchen.h"
-#include "../Floor/Person.h"
+
 using namespace std;
 int main(){
-    Person p;
     Kitchen k;
-
-    p.sayHello();
+    
     k.sayHello();
 }
