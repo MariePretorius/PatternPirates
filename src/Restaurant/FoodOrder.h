8c8402ab60ff012ec607e2987231d982c09b7bef
shan_226 <shannonventer26@gmail.com>
2023-11-01 19:44:16 +0200

    FoodOrder header changed

--- src/Restaurant/FoodOrder.h
+++ src/Restaurant/FoodOrder.h
@@ -2,27 +2,36 @@
 #define FOODORDER_H
 
 class Customer;
+class Kitchen;
+class Bill;
+
+//#include "../Kitchen/Kitchen.h"
+//#include "../Floor/Bill.h"
 #include <iostream>
 
-class FoodOrder {
+class FoodOrder 
+{
+	private:
+		std::string* ingredients;
+		int numberOfIngredients;
+		std::string cookingMethod;
+		int tableNumber;
+		Customer* customer;
+		Kitchen& kitchen;
+		Bill& bill;
+		double* prices;
 
-private:
-	std::string ingredients;
-	std::string cookingMethod;
-	int tableNumber;
-	Customer * customer;
+		// prices map
 
-public:
-	FoodOrder(std::string ingredients, std::string method, std::string tableNumber, Customer& customer);
-
-	FoodOrder();
-
-	void execute();
-
-	void addToBill(std::string ingredient);
-
-	std::string getIngredients();
+	public:
+		FoodOrder(std::string* ingredients, double* prices, int num, std::string method, int tableNumber, Customer& customer, Kitchen& k, Bill& bill);
+		~FoodOrder();
+		void execute();
+		void addToBill(std::string ingredient, double cost);
+		std::string* getIngredients();
+		std::string getCookingMethod();
+		int getTableNumber();
+		Customer* getCustomer();
 };
-
 
 #endif
