fdca41f0dd39f76e7733917a4495b7c768c02be9
shan_226 <shannonventer26@gmail.com>
2023-11-03 18:46:02 +0200

    changed constructor and added a setter

--- src/Restaurant/FoodOrder.cpp
+++ src/Restaurant/FoodOrder.cpp
@@ -1,27 +1,193 @@
-//
-// Created by Tristan on 2023/10/30.
-//
-
 #include "FoodOrder.h"
 #include "../Floor/Customer.h"
-#include <list>
+#include "../Floor/Bill.h"
+#include "../Kitchen/Kitchen.h"
 
-FoodOrder::FoodOrder(std::string ingredients, std::string method, std::string tableNumber, Customer &customer) {
+/**
+ * @brief Default constructor for the class.
+*/
+FoodOrder :: FoodOrder() {}
+
+/**
+ * Parameterized constructor for the class.
+ * @param ingredients List of ingredients in the order.
+ * @param prices List of prices for the ingredients.
+ * @param num Number of ingredients in order.
+ * @param method The cooking method of the order.
+ * @param tableNumber Table number the order came from.
+ * @param customer Reference to the customer who ordered this.
+ * @param k Reference to the kitchen object.
+ * @param bill Reference to the bill for this order.
+*/
+FoodOrder :: FoodOrder(std::vector<std::string> ingredients, std::vector<double> prices, int num, std::string method, int tableNumber, Customer& customer, Bill* bill) : bill(bill)
+{
     this->customer = &customer;
+    //this->kitchen = &k;
+    //this->bill = &bill;
+    this->numberOfIngredients = num;
+    this->cookingMethod = method;
+    this->tableNumber = tableNumber;
+    //this->ingredients = new std::string[numberOfIngredients];
+    //this->prices = new double[numberOfIngredients];
+    for (int i = 0; i < numberOfIngredients; i++)
+    {
+        //this->ingredients[i] = ingredients[i];
+        this->ingredients.push_back(ingredients[i]);
+        //this->prices[i] = prices[i];
+        this->ingredients.push_back(ingredients[i]);
+        
+        addToBill(this->ingredients[i], this->prices[i]);
+    }
 }
 
-FoodOrder::FoodOrder() {
-
+/**
+ * @brief Destructor for this class.
+*/
+FoodOrder :: ~FoodOrder()
+{
+    //delete ingredients;
+    //delete prices;
 }
 
-void FoodOrder::execute() {
-
+/**
+ * @brief Execute function to pass order to kitchen.
+*/
+void FoodOrder :: execute()
+{
+    kitchen->addNewOrder(*this);
 }
 
-void FoodOrder::addToBill(std::string ingredient) {
-
+/**
+ * @brief Function to add all the ingredients onto the bill.
+ * @param ingredient Single ingredient to add to bill.
+ * @param cost Cost of the ingredient being added.
+*/
+void FoodOrder :: addToBill(std::string ingredient, double cost)
+{
+    bill->addItem(ingredient, cost);
 }
 
-std::list<std::string> FoodOrder::getIngredients() {
-    return ingredients;
+/**
+ * @brief Returns the ingredient list.
+ * @return Returns a vector of ingredients.
+*/
+std::list<std::string> FoodOrder :: getIngredients()
+{
+    return this->ingredients;
 }
+
+/**
+ * @brief Returns the cooking method.
+ * @return Returns a string of the cooking method.
+*/
+std::string FoodOrder :: getCookingMethod()
+{
+    return this->cookingMethod;
+}
+
+/**
+ * @brief Returns the table number the order belongs to.
+ * @return Returns integer value of the table number.
+*/
+int FoodOrder :: getTableNumber()
+{
+    return this->tableNumber;
+}
+
+/**
+ * @brief Returns a pointer to the customer the order belongs to.
+ * @return Customer object pointer.
+*/
+Customer* FoodOrder :: getCustomer()
+{
+    return this->customer;
+}
+
+/**
+ * @brief Returns a random id between 1000 and 9999.
+ * @return Return an integer value for the id.
+*/
+int FoodOrder :: getRandomID()   //add vector if necessary
+{
+    std::srand(static_cast<unsigned int>(time(nullptr)));
+    int randomNumber = std::rand() % 9000 + 1000;
+
+    return randomNumber;
+
+    // std::vector<int> ids;
+    // int k = 1111;
+    // bool found = false;
+    // bool done = false;
+    // int randomNumber = 0;
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     ids.push_back(k);
+    //     k++;
+    // }
+
+    // do
+    // {
+    //     //std::srand(static_cast<unsigned int>(time(nullptr)));
+    //     randomNumber = std::rand() % 9000 + 1000;
+
+    //     auto it = std::find(ids.begin(), ids.end(), randomNumber);
+
+    //     if (it != ids.end()) 
+    //     {
+    //         found = true;
+    //     } 
+
+    //     // for (int i = 0; i < ids.size(); i++)
+    //     // {
+    //     //     if (ids[i] == random)
+    //     //     {
+    //     //         found = true;
+    //     //         break;
+    //     //     }
+    //     // }
+    // }
+    // while (found == true);
+
+    // ids.push_back(randomNumber);
+    
+    // return randomNumber;
+}
+
+/**
+ * @brief Sets the kitchen pointer.
+ * @param k Pointer to a kitchen object.
+*/
+void FoodOrder :: setKitchen(Kitchen* k)
+{
+    this->kitchen = k;
+}
+
+// In Waiter: 
+// void takeOrder()
+// {
+//     // order parameters : 
+//     // std::string* ingredients : array of ingredients
+//     // double* prices : array of prices corresponding to the array of ingredients
+//     // int num : number of ingredients/prices
+//     // std::string method : cooking method
+//     // int tableNumber : table number corresponding to the order
+//     // Customer& customer : customer whose order it is
+//     // Kitchen& k : reference to the kitchen
+//     // Bill& bill : reference to the bill [used to add the order onto the bill]
+
+//     // contents of function
+//     FoodOrder order(ingredients, prices, num, method, tableNumber, customer, kitchen, bill);
+
+//     order.execute();
+// }
+
+// In Kitchen
+// void prepareFood(const std::string* ingredients, std::string cookingMethod) 
+// {
+//     // set cooking method
+//     // add ingredients
+    
+//     // start process basically
+//     // add to queue
+// }
--- src/Restaurant/FoodOrder.h
+++ src/Restaurant/FoodOrder.h
@@ -1,29 +1,106 @@
 #ifndef FOODORDER_H
 #define FOODORDER_H
 
+#include "OrderCommand.h"
+
 class Customer;
+class Kitchen;
+class Bill;
+
+//#include "../Kitchen/Kitchen.h"
+//#include "../Floor/Bill.h"
 #include <iostream>
+#include <cstdlib>
+#include <algorithm>
 #include <list>
 
-class FoodOrder {
+/**
+ * @class FoodOrder Class
+ * This class is the ConcreteCommand participant in the Command design pattern. It Inherits from the OrderCommand class.
+*/
+class FoodOrder : public OrderCommand
+{
+	private:
+		std::vector<std::string> ingredients;  ///< Vector containing the ingredients in the order.
+		int numberOfIngredients;  ///< Integer containing the number of ingredients in the order.
+		std::string cookingMethod;  ///< String containing the cooking method of the order.
+		int tableNumber;  ///< Integer containing the table number this order belongs to.
+		Customer* customer;  ///< Reference to the customer that ordered this.
+		Kitchen* kitchen;  ///< Reference to the Kitchen object.
+		Bill* bill;  ///< Reference to the Bill object of this order.
+		std::vector<double> prices;  ///< Vector containing the prices of the ingredients of this order.
+		int id; ///< Integer used to identify the order.
 
-private:
-	std::list<std::string> ingredients;
-	std::string cookingMethod;
-	int tableNumber;
-	Customer * customer;
+	public:
+		/**
+		 * @brief Default constructor for the class.
+		*/
+		FoodOrder();
 
-public:
-	FoodOrder(std::string ingredients, std::string method, std::string tableNumber, Customer& customer);
+		/**
+		 * Parameterized constructor for the class.
+		 * @param ingredients List of ingredients in the order.
+		 * @param prices List of prices for the ingredients.
+		 * @param num Number of ingredients in order.
+		 * @param method The cooking method of the order.
+		 * @param tableNumber Table number the order came from.
+		 * @param customer Reference to the customer who ordered this.
+		 * @param bill Reference to the bill for this order.
+		*/
+		FoodOrder(std::vector<std::string> ingredients, std::vector<double> prices, int num, std::string method, int tableNumber, Customer& customer, Bill* bill);
+		
+		/**
+		 * @brief Destructor for this class.
+		*/
+		~FoodOrder();
 
-	FoodOrder();
+		/**
+		 * @brief Execute function to pass order to kitchen.
+		*/
+		void execute();
 
-	void execute();
+		/**
+		 * @brief Function to add all the ingredients onto the bill.
+		 * @param ingredient Single ingredient to add to bill.
+		 * @param cost Cost of the ingredient being added.
+		*/
+		void addToBill(std::string ingredient, double cost);
 
-	void addToBill(std::string ingredient);
+		/**
+		 * @brief Returns the ingredient list.
+		 * @return Returns a vector of ingredients.
+		*/
+		std::vector<std::string> getIngredients();
 
-	std::list<std::string> getIngredients();
+		/**
+		 * @brief Returns the cooking method.
+		 * @return Returns a string of the cooking method.
+		*/
+		std::string getCookingMethod();
+
+		/**
+		 * @brief Returns the table number the order belongs to.
+		 * @return Returns integer value of the table number.
+		*/
+		int getTableNumber();
+
+		/**
+		 * @brief Returns a pointer to the customer the order belongs to.
+		 * @return Customer object pointer.
+		*/
+		Customer* getCustomer();
+
+		/**
+		 * @brief Returns a random id between 1000 and 9999.
+		 * @return Return an integer value for the id.
+		*/
+		int getRandomID();  //can add vecter imput to check against duplicates
+
+		/**
+		 * @brief Sets the kitchen pointer.
+		 * @param k Pointer to a kitchen object.
+		*/
+		void setKitchen(Kitchen* k);
 };
-
 
 #endif
