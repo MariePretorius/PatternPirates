1eace332ff974101c20b0702a0a8e1d63e2c447d
BubbleQueenzy <u21434558@tuks.co.za>
2023-11-04 13:00:06 +0200

    added Doxygen comments to Restaurant.h

--- src/Restaurant/Restaurant.h
+++ src/Restaurant/Restaurant.h
@@ -6,34 +6,50 @@
 #include "../Kitchen/Kitchen.h"
 #include <iostream>
 #include "../Floor/Host.h"
-//hellos
 using namespace std;
+/**
+ * @class Restaurant
+ * @brief Restaurant is a facade and a singleton. It is responsible for the game loop.
+ */
 class Restaurant {
 
 private:
-	static Restaurant* uniqueInstance;
-    Finance* funds;
-    Floor* floor;
-    Kitchen* kitchen;
+	static Restaurant* uniqueInstance; ///<unique instance part of singleton design pattern
+    Finance* funds; ///< a reference to the funds of the restaurant
+    Floor* floor; ///< reference to the floor of the restaurant
+    Kitchen* kitchen; ///<reference to the kitchen of the restaurant
 public:
+    /**
+     * instance is part of the singleton pattern it ensures that there is only one instance of Restaurant
+     * @return returns the unuqueInstance
+     */
 	static Restaurant* instance();
 
 protected:
+    /**
+     * Default constructor made protected so that outside classes can't call it
+     */
 	Restaurant();
 
+    /**
+     * Default copy constructor made protected so that outside classes can't call it
+     */
 	Restaurant(Restaurant& restaurant);
-
+    /**
+     * Operator overloading of operator== made protected so that outside classes can't call it
+     */
 	void operator=(Restaurant& restaurant);
 
 public:
+    /**
+     * setup instantiates the kitchen, funds and floor. It also gets user input to populate the shelf with ingredients.
+     */
 	void setup();
 
+    /**
+     * simulate is responsible for the game loop , calling all the necessary functions from floor and kitchen
+     */
 	void simulate();
 
-    double getFunds();
-
-    void subtractFunds(double x);
-
-    //Kitchen* getKitchen();
 };
 #endif
