0b2b9fd483554cd81eeb38725b97f6f08f84d502
Hucket_Bat <tristanpotgieter2001@gmail.com>
2023-11-06 19:25:14 +0200

    Added doxygen comments

--- src/Kitchen/Cook.h
+++ src/Kitchen/Cook.h
@@ -1,6 +1,14 @@
+/**
+ * @file Cook.h
+ * @brief Declaration of the Cook class.
+ */
 #ifndef COOK_H
 #define COOK_H
 class Ingredient;
+/**
+ * @class Cook
+ * @brief Represents a Strategy that is used to define a fryer and a griller strategy.
+ */
 class Cook {
 
 public:
